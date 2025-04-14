37dc69270f52d793828ad5a97583bda8a7d70ac5
VINAY9017 <vinayraj9017@gmail.com>
2025-04-14 15:52:13 +0530

    final

---
+++ src/Middleware/tokenMiddleware.js
@@ -0,0 +1,27 @@
+
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+
+exports.blogMiddleware = (request, response, next) => {
+    try {
+        const token = request.headers["authorization"].split(" ")[1];
+        const verified = jwt.verify(token, process.env.SECRET_KEY)
+        if (verified) {
+            request.id=verified.user_id,
+            next()
+        }
+        else {
+            response.json({
+                status: "failed",
+                message: "unauthorized token was expired"
+            })
+        }
+    }
+    catch(error) {
+        console.log(error)
+        response.json({
+            status: "failed",
+            message: "unauthorized token"
+        })
+    }
+}
